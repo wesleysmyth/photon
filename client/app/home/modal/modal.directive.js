f0be8e57cd0a6c970910c531f02fb6888542fdfa
Wes <wesleytatesmith@gmail.com>
2015-04-07 12:56:02 -0400

    Adds use strict statements to directives

--- client/app/home/modal/modal.directive.js
+++ client/app/home/modal/modal.directive.js
@@ -1,5 +1,6 @@
 (function() {
-  
+  'use strict';
+
   angular
     .module('app.modal')
     .directive('modal', modal);
--- client/app/home/sidebar/sidebar.directive.js
+++ client/app/home/sidebar/sidebar.directive.js
@@ -1,5 +1,6 @@
 (function() {
-  
+  'use strict';
+
   angular
     .module('app.sidebar')
     .directive('sidebar', sidebar);
